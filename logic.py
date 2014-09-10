59ab51cd0994875a5344e90f9f7a80662af7aee0
christian kalhauge <christian@kalhauge.dk>
2014-09-10 13:45:58 +0200

    Cleaned up directory andded tests and docs

--- logic.py
+++ logic.py
@@ -1,4 +1,9 @@
 """
+Logic
+=====
+
+.. currentmodule:: pysmt.logic
+
 This file descripes the fundamental partial order logic, as used in this module.
 """
 
@@ -7,7 +12,31 @@ import itertools
 from abc import abstractmethod
 from collections import namedtuple
 
-Literal = namedtuple('Literal', ['name', 'type'])
+class Literal(namedtuple('Literal', ['name', 'type_', 'value'])):
+    """
+    A litteral is the values that we want to solve. The standart of this
+    literals are that they have to fulfill the SMTLIB specs, this means that the
+    name and the type_ must be according to thier specifications.
+
+    :param str name: The name of the litteral.
+    :param str type_: The type of the literal described by a string.
+    :param str value: The value that is litteral is associated with. This is
+        used for traceability.
+    """
+
+    @staticmethod
+    def literalize(value, type_):
+        """
+        Takes a value and makes it into a literal remembering the original
+        value. If the value has a method called :meth:`rep`, this is called to
+        generate the solution, else a name is generated from the hash value.
+        """ 
+        try: name = value.rep()
+        except AttributeError: 
+            name = 'l' + hash(value)
+
+        return Literal(name, type_, value)
+
 
 class Term:
     """ A piece of logic """
@@ -17,6 +46,13 @@ class Term:
         """ Returns the literals used in the term """
 
 class All(Term):
+    """ 
+    All subterms must be true.
+   
+    .. attribute:: terms 
+        
+        The subterms that must be true. 
+    """
 
     def __init__(self, terms):
         self.terms = list(terms) 
@@ -29,6 +65,13 @@ class All(Term):
         )
 
 class Any(Term):
+    """ 
+    Any of the subterms must be true.
+   
+    .. attribute:: terms 
+    
+        The subterms that must be true. 
+    """
 
     def __init__(self, terms):
         self.terms = list(terms)
@@ -41,6 +84,13 @@ class Any(Term):
         )
 
 class Order(Term):
+    """ 
+    All the literals must be ordered.
+   
+    .. attribute:: ordered_literals
+
+        The literals must be ordered and givven values from small to large.
+    """
     
     def __init__(self, literals):
         self.ordered_literals = list(literals)
@@ -51,6 +101,7 @@ class Order(Term):
 class Next(Term):
     """
     A next term is true if the O(e1) + 1 = O(e2)
+
     """
     
     def __init__(self, e1, e2):
