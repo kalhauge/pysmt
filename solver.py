64d4d569a6bd74a5059b225ad4a070862e78f258
christian kalhauge <christian@kalhauge.dk>
2014-10-22 16:48:50 +0200

    Added real imporvements to the program speed

--- solver.py
+++ solver.py
@@ -10,6 +10,9 @@ Solver
 """
 from abc import abstractmethod
 import re
+from subprocess import Popen, PIPE, STDOUT
+import itertools
+import time
 
 import logging 
 log = logging.getLogger('pysmt.solver')
@@ -28,40 +31,112 @@ class Solver:
     def satisfy(self, term):
         """ 
         Returns a dictionary of values satisfying the term ordered by
-        the values of the litterals, or an :class:`UnsatisfiableTerm` exception.
+        the values of the literals, or an :class:`UnsatisfiableTerm` exception.
         """
 
 class SMT2Solver (Solver):
 
-    re_return = re.compile('^(sat|unsat)$', re.MULTILINE)
+    re_return = re.compile('(sat|unsat)')
     re_function = re.compile('\((?P<name>[^ \(\)]*) (?P<value>.*)\)')
+    re_solution = re.compile(r'''
+        \(
+            ( \(
+                [^\(\)]*
+                ( 
+                    \(- [^\(\)]*\) #Negative values
+                    | [^\(\)]*
+                ) 
+            \) \s*)*
+        \)
+    ''', re.VERBOSE | re.MULTILINE)
     re_number = re.compile('(\(- (?P<minus>[0-9]+)\))|(?P<plus>[0-9]+)')
-
+    
     def __init__(self, logic="QF_IDL"):
-        self.count = 0
         self.logic = logic 
     
-    def commands(self, term, literals=None): 
-        if not literals:
-            literals = list(term.literals())
-        return [
+    def satisfiable(self, context, terms, solutions=False):
+        """ For each term test if they are satitisfiable in the context of the
+        world.
+        """
+        log.debug('Starting solveing terms using a context of size %s', context.size())
+        literals = set(context.literals())
+        with self.open():
+            self.preable()
+            self.declare_functions(literals)
+            self.assert_term(context)
+            for term in terms:
+                log.debug('Solving Term of size %s.', term.size())
+                term_literals = set(term.literals())
+                self.send(['(push 1)'])
+                self.declare_functions(term_literals - literals)
+                self.assert_term(term)
+                satisfied = self.has_solution()
+                log.debug('Solution %s.', 'found' if satisfied else 'not found')
+                if solutions:
+                    if satisfied:
+                        yield self.get_solution(literals | term_literals)
+                    else:
+                        yield None
+                else:
+                    yield satisfied
+                self.send(['(pop 1)'])
+
+    def satisfy(self, term):
+        log.debug('Satisfing term of size %s', term.size())
+        literals = set(term.literals())
+        with self.open():
+            self.preable()
+            self.declare_functions(literals)
+            self.assert_term(term)
+            satisfied = self.has_solution()
+            log.debug('Solution %s.', 'found' if satisfied else 'not found')
+            if satisfied:
+                return self.get_solution(literals)
+            else:
+                raise UnsatisfiableTerm()
+
+    def filter(self, terms, context):
+        test_terms, terms = itertools.tee(terms)
+        tests = self.satisfiable(context, test_terms)
+        return (term for term, test in zip(terms, tests) if test)
+
+    def preable(self):
+        self.send([
             '(set-option :produce-models true)',
             '(set-logic {})'.format(self.logic)
-        ] + [
+        ])
+        
+    def declare_functions(self, literals):
+        self.send([
             '(declare-fun {0.name} () {0.type_})'.format(literal) 
             for literal in literals  # sorted(literals, key=lambda x:x.name)
-        ] + [
-            #'(assert (!= {}))'.format(' '.join(l.name for l in literals)),
-            '(assert {})'.format(self.compile(term))
-        ] + [ 
-            '(check-sat)',
-            '(get-value ({}))'.format(' '.join(
-                literal.name for literal in literals 
-            ))
-        ]
+        ])
+
+    def assert_term(self, term):
+        self.send([ '(assert {})'.format(self.compile(term)) ])
+
+    def get_solution(self, literals):
+        self.send([
+            '(get-value ({}))'.format(
+                ' '.join(literal.name for literal in literals)
+        )])
+        output = self.recv(self.re_solution)
+        mapping = {}
+        for match in self.re_function.finditer(output):
+            value, name = match.group('value', 'name')
+            if name == 'error':
+                raise Exception(value)
+            d = self.re_number.match(value).groupdict()
+            value = -int(d['minus']) if d['minus'] else int(d['plus'])
+            mapping[match.group('name')] = value
+
+        solution = {}
+        for literal in literals:
+            solution[literal.value] = mapping[literal.name]
+
+        return solution
 
     def compile(self, term, depth=0):
-        self.count += 1
         indent = ('\n' + ' ' * (4 * (depth + 1)))
         if isinstance(term, logic.All):
             return '(and {})'.format(
@@ -88,63 +163,88 @@ class SMT2Solver (Solver):
                 "Don't know how to handle {}.".format(term)
             )
 
-    def parse(self, output):
-        mo = self.re_return.match(output)
-        if mo is None:
-            raise Exception('Bad compiling, {}'.format(output))
-        elif mo.group(0) != 'sat':
-            raise UnsatisfiableTerm(output)
 
-        solution = {}
-        for match in self.re_function.finditer(output):
-            value, name = match.group('value', 'name')
-            if name == 'error':
-                raise Exception(value)
-            d = self.re_number.match(value).groupdict()
-            value = -int(d['minus']) if d['minus'] else int(d['plus'])
-            solution[match.group('name')] = value
+    def has_solution(self):
+        self.send(['(check-sat)'])
+        solution = self.recv(self.re_return)
+        return solution == 'sat'
 
-        return solution
+    def __enter__(self):
+        if not self.is_open: 
+            self.open()
+        return self
 
-    def satisfy(self, term):
-        log.debug('Satisfing term of size %s', term.size())
-        commands = self.commands(term)
-        self.count = 0
-        try:
-            cmd, output = self.run_commands(commands)
-            mapping = self.parse(output)
-            solution = {}
-            for literal in term.literals():
-                solution[literal.value] = mapping[literal.name]
-            log.debug('Solution found, %s literals mapped', len(solution))
-            return solution
-        except UnsatisfiableTerm:
-            log.debug('Solution NOT found')
-            raise
-        except CalledProcessError as e:
-            log.error('CalledProcessError: %s', e.cmd)
-            log.error(e.output)
-            raise
-        except Exception as e:
-            log.error(e)
-            raise
+    def __exit__(self, type, value, tb):
+        self.close()
 
     @abstractmethod
-    def run_commands(self, term):
+    def send(self, command):
         """ Runs the commands and returns the output."""
+
+    @abstractmethod
+    def recv(self):
+        """ Recieves a response """
+
+    @abstractmethod
+    def open(self):
+        """ Opens up a connection to the solver, needs to return self""" 
+
+    @abstractmethod
+    def close(self):
+        """ Closes the solver """
 
 class Yices (SMT2Solver):
 
-    def run_commands(self, commands):
-        from subprocess import check_output
-        filename, out = ensurefile()
-        with out:
-            for command in commands:
-                print(command, file=out)
-        return ['yices-smt2', filename], check_output(
-            ['yices-smt2', filename],
-            universal_newlines=True
+    def __init__(self, mode='QF_IDL'):
+        self.process = None
+        self.tempfile = None
+        self.readbuffer = ''
+        self.index = 0
+        super().__init__(mode)
+
+    def send(self, commands):
+        for command in commands:
+            print(command, file=self.tmpfile)
+            print(command, file=self.process.stdin)
+        self.process.stdin.flush()
+
+    def recv(self, re=None):
+        match = None
+        counter = 0
+        for line in self.process.stdout:
+            self.readbuffer += line
+            counter += line.count('(') - line.count(')')
+            if counter <= 0:
+                break        
+             
+            ## match = re.search(self.readbuffer, pos=self.index)
+        match = self.readbuffer[self.index:]
+        self.index = len(self.readbuffer) ## match.end(0)
+        search = re.search(match).group(0)
+        return search
+
+    def open(self):
+        self.index = 0
+        self.readbuffer = ''
+        filename, self.tmpfile = ensurefile()
+        self.process = Popen(
+            ['yices-smt2','--incremental'],
+            universal_newlines=True,
+            stdout=PIPE,
+            stderr=STDOUT,
+            stdin=PIPE,
         )
+        return self
+
+    def is_open(self):
+        return not self.process is None
+
+    def close(self):
+        if self.is_open(): 
+            self.process.stdin.close()
+            self.process.terminate()
+            self.tmpfile.close()
+            self.process = None
 
 def ensurefile(filename=None):
     if filename: 
---
+++ test/test_pysmt.py
@@ -0,0 +1,65 @@
+
+import pysmt 
+import itertools
+
+solver = pysmt.solver.Yices('QF_IDL')
+
+class Test():
+    
+    def __init__(self, id):
+        self.id = id
+   
+    @property 
+    def _rep(self):
+        return 't' + str(self.id)
+
+    def __repr__(self):
+        return self._rep
+
+def test_yices_statisfy():
+    t1, t2 = Test(0), Test(1)
+    solution = solver.satisfy(pysmt.logic.order(t1,t2))
+    assert solution[t1] < solution[t2]
+
+def test_yices_statifiable():
+    t1, t2, t3 = Test(1), Test(2), Test(3)
+    context = pysmt.all([
+        pysmt.logic.order(t1, t3),
+        pysmt.logic.order(t2, t3),
+    ])
+
+    tests, other = itertools.tee(
+        pysmt.logic.order(a, b) for a, b in 
+            itertools.permutations([t1, t2, t3], 2)
+    )
+
+    assert set( test for sat, test in 
+        zip(solver.satisfiable(context, tests), other) if sat
+    ) == set(pysmt.logic.order(a, b) for a, b in [
+        (t1, t3),
+        (t2, t3),
+        
+        (t1, t2),
+        (t2, t1),
+    ])
+
+
+def test_yices_filter():
+    t1, t2, t3 = Test(1), Test(2), Test(3)
+    context = pysmt.all([
+        pysmt.logic.order(t1, t3),
+        pysmt.logic.order(t2, t3),
+    ])
+
+    terms = (
+        pysmt.logic.order(a, b) for a, b in 
+        itertools.permutations([t1, t2, t3], 2)
+    )
+
+    assert set(solver.filter(terms, context)) == set(pysmt.logic.order(a, b) for a, b in [
+        (t1, t3),
+        (t2, t3),
+        
+        (t1, t2),
+        (t2, t1),
+    ])
